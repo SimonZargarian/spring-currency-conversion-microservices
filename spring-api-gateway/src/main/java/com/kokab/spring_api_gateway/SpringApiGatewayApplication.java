5e46dfe1df5d460246ec9042cb53a3d8b5da1ca1
Simon Zargarian <simonzargariancv@gmail.com>
2024-05-31 15:12:27 +0200

    Initial commit

---
+++ spring-api-gateway/src/main/java/com/kokab/spring_api_gateway/SpringApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.kokab.spring_api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApiGatewayApplication.class, args);
+	}
+
+}
